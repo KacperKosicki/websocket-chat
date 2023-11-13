82dd7ac550fe710ce1c02c41b26a7707b44c5847
Kacper Kosicki <kosickikacper1@gmail.com>
2023-11-13 08:55:08 +0100

    Added the ability to write in real time using Websocket

--- client/app.js
+++ client/app.js
@@ -1,11 +1,14 @@
-let userName = '';
+// app.js
 
+let userName = '';
 const loginForm = document.getElementById('welcome-form');
 const messagesSection = document.querySelector('.messages-section');
 const messagesList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
+
+const socket = io();
 
 function addMessage(author, content) {
   const message = document.createElement('li');
@@ -33,6 +36,8 @@ function login(event) {
 
   loginForm.classList.remove('show');
   messagesSection.classList.add('show');
+
+  socket.emit('join', userName);
 }
 
 function sendMessage(event) {
@@ -46,9 +51,14 @@ function sendMessage(event) {
 
   addMessage(userName, messageContent);
 
+  socket.emit('message', { author: userName, content: messageContent });
+
   messageContentInput.value = '';
 }
 
 loginForm.addEventListener('submit', login);
-
 addMessageForm.addEventListener('submit', sendMessage);
+
+socket.on('message', ({ author, content }) => addMessage(author, content));
+socket.on('newUser', (user) => addMessage('Chat Bot', `${user.name} has joined the conversation!`));
+socket.on('removeUser', (user) => addMessage('Chat Bot', `${user.name} has left the conversation... :(`));
--- server.js
+++ server.js
@@ -1,15 +1,26 @@
+// server.js
+
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 const port = 8000;
 
 app.use(express.static(path.join(__dirname, 'client')));
 
 const messages = [];
+const users = [];
 
 app.get('/messages', (req, res) => {
   res.json(messages);
+});
+
+app.get('/users', (req, res) => {
+  res.json(users);
 });
 
 app.post('/messages', express.json(), (req, res) => {
@@ -18,10 +29,44 @@ app.post('/messages', express.json(), (req, res) => {
   res.status(201).json(newMessage);
 });
 
+app.post('/users', express.json(), (req, res) => {
+  const newUser = req.body;
+  users.push(newUser);
+  io.emit('newUser', newUser);
+  res.status(201).json(newUser);
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+
+  socket.on('message', (message) => {
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.on('join', (userName) => {
+    const user = { name: userName, id: socket.id };
+    users.push(user);
+    io.emit('newUser', user);
+  });
+
+  socket.on('disconnect', () => {
+    const disconnectedUser = users.find(user => user.id === socket.id);
+    if (disconnectedUser) {
+      io.emit('removeUser', disconnectedUser);
+      users.splice(users.indexOf(disconnectedUser), 1);
+    }
+    console.log('Oh, socket ' + socket.id + ' has left');
+  });
+
+  console.log('I\'ve added listeners on message, join, and disconnect events \n');
+});
+
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
